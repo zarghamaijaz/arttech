aed2b0fd9bcda6d6b8540344ff1f70d6dc14c82d
zarghamaijaz <zaijaz@vaporvm.com>
2025-04-23 17:44:44 +0500

    Bugs fixed while scrolling

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ function animateBlobs(){
         const blob3 = document.querySelector("#blob-3");
         const blackCricle = document.querySelector("#black-circle");
 
+        const windowWidth = window.innerWidth;
+        const windowheight = window.innerHeight;
+        const highest = Math.max(windowWidth, windowheight);
+        blackCricle.style.height = `${highest}px`;
+        blackCricle.style.width = `${highest}px`;
+
+        // console.log(windowWidth);
+        // console.log(windowheight);
+
         const  blobFloat1 = gsap.to(blob1, {
           duration: 10,
           x: 120,
@@ -50,49 +59,60 @@ function animateBlobs(){
           .addTo(controller);
           let lastProgress = 0;
         scene.on("progress", function (e) {
-            requestAnimationFrame(() => {
-                const p = e.progress;
-                if(p === 0){
-                    blobFloat1.play();
-                    blobFloat2.play();
-                    blobFloat3.play();
-                }
-                else{
-                    blobFloat1.pause();
-                    blobFloat2.pause();
-                    blobFloat3.pause();
-                }
-                if(p < 0.5){
-                    const transformPercent = Math.ceil(p / 0.5 * 100);
-                    gsap.to(blob1, {
-                        x: `-${transformPercent}%`,
-                    });
-                    gsap.to(blob2, {
-                        x: `-${transformPercent}%`,
-                    });
-                    gsap.to(blob3, {
-                        x: `${transformPercent}%`,
-                    });
-                    gsap.to(content, {
-                        scale: (p) + 1,
-                        opacity: 1 - p * 2,
-                    })
-                }
-                else if(p > 0.5){
-                    let transformPercent = (p - 0.5) * 2;
-    
-                    if(transformPercent < 0.05){
-                        transformPercent = 0;
-                    }
-                    else if(transformPercent > 0.99){
-                        transformPercent = 1.11;
-                    }
-                    console.log(transformPercent);
-                    
-                    blackCricle.style.transform = `scale(${transformPercent})`;
-                }
-                lastProgress = p;
+          let p = e.progress;
+          // Snap if very close to edges
+          if (p < 0.05) p = 0;
+          if (p > 0.95) p = 1;
+          if(p === 0){
+              blobFloat1.play();
+              blobFloat2.play();
+              blobFloat3.play();
+          }
+          else{
+              blobFloat1.pause();
+              blobFloat2.pause();
+              blobFloat3.pause();
+          }
+          if(p < 0.5){
+            blackCricle.style.transform = `scale(0)`;
+            const transformPercent = Math.ceil(p / 0.5 * 100);
+            gsap.to(blob1, {
+              x: `-${transformPercent}%`,
+            });
+            gsap.to(blob2, {
+              x: `-${transformPercent}%`,
+            });
+            gsap.to(blob3, {
+              x: `${transformPercent}%`,
+            });
+            gsap.to(content, {
+              scale: (p) + 1,
+              opacity: 1 - (p * 2),
             })
+          }
+          else if(p > 0.5){
+            gsap.to(blob1, {
+              x: `-100%`,
+            });
+            gsap.to(blob2, {
+              x: `-100%`,
+            });
+            gsap.to(blob3, {
+              x: `100%`,
+            });
+            gsap.to(content, {
+              scale: 1,
+              opacity: 0,
+            })
+            const transformPercent = (p-0.5) * 2;
+            console.log(p, "Progress")
+            console.log(transformPercent);
+            
+            blackCricle.style.transform = `scale(${transformPercent * 1.11})`;
+          }
+          lastProgress = p;
+          // requestAnimationFrame(() => {
+          // })
             
         });
     }
